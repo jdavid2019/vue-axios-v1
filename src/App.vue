97ed8fbfb8ef87673c4314f8a2517d213dab8f25
jdavid2019 <joseph2019d@gmail.com>
2021-04-29 22:02:36 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EAxio />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EAxio from './components/EAxio.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    EAxio
   }
 }
 </script>
---
+++ src/components/EAxio.vue
@@ -0,0 +1,47 @@
+<template>
+   <div>
+  <p v-for="item in all" :key="item.id"  >
+    {{item.id}} - {{item.title}} - <b v-bind:class="item.completed==false?'red':'green'">{{item.completed}}</b>
+  </p>
+   </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+name: 'EAxio',
+  data(){
+  return{
+    all: ''
+  }
+  },
+  mounted() {
+  console.log('start...')
+  this.getData();
+  },
+  methods: {
+    getData(){
+      console.log('enter to the method')
+      axios.get('https://jsonplaceholder.typicode.com/todos').then(
+          response => {
+            console.log(response)
+            this.all = response.data
+          }
+      ).catch(
+          e => console.log(e)
+      )
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+.red{
+  color: red;
+}
+.green{
+  color: green;
+}
+</style>
